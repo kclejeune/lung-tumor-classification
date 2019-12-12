3e370856b0b2c83c8c0db55f7a6bbcfa33c9b263
Sam Jenkins <soj3@case.edu>
2019-12-12 17:53:18 -0500

    something

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import convolutional
 import torch
+import torch.multiprocessing as mp
+
+
+# TODO: define train set, test set, and validation set from input data
+def main():
+    print("Live fast, eat ass")
+    pass
+
+
+if __name__ == '__main__':
+    main()
